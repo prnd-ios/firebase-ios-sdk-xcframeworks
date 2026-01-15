cbcdaf9ebe42947382b239ae9b4a31d3db2f5caf
wani <wani@prnd.co.kr>
2026-01-15 16:13:03 +0900

    feat: Firebase iOS 배포 자동화 스크립트 개선
    Firebase iOS SDK를 패키징하고 배포하는 스크립트를 개선한다.
    
    - 필요한 모듈만 필터링하고 iOS 플랫폼만 남기도록 설정한다.
    - SPM에서 Firebase를 사용할 수 있도록 dummy 파일 및 umbrella header를 추가한다.
    - xcframeworks 리포지토리를 prnd-ios로 변경한다.
    
    Merge pull request #1 from prnd-ios/feature/prnd-target
    
    feat: Firebase iOS 배포 자동화 스크립트 개선
    
    Auto stash before rebase of "feature/prnd-target" onto "master"

--- .scripts/package_template.swift
+++ .scripts/package_template.swift
@@ -1,11 +1,11 @@
-// swift-tools-version:5.3
+// swift-tools-version:5.9
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
   name: "Firebase",
-  platforms: [.iOS(.v11), .macOS(.v10_12), .tvOS(.v12), .watchOS(.v7)],
+  platforms: [.iOS(.v15)],
   products: [
 // GENERATE LIBRARIES
   ],
