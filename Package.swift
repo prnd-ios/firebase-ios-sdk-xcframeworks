f612cd7546d3c132a0599e110364e385535b3095
wani <wani@prnd.co.kr>
2026-01-16 12:14:30 +0900

    fix: Update checksums to match actual release files

--- Package.swift
+++ Package.swift
@@ -127,122 +127,122 @@ let package = Package(
     .binaryTarget(
       name: "_AppCheckCore",
       url: "https://github.com/prnd-ios/firebase-ios-sdk-xcframeworks/releases/download/12.8.0/_AppCheckCore.xcframework.zip",
-      checksum: "7628decb1b5fbb4a97c609321071c18b330e336065c8f20c29454927c951cf7d"
+      checksum: "9946e2c5cfc3ac13cee0f6a62cefdbe0fc077c8702d91678908b412a5d69d079"
     ),
     .binaryTarget(
       name: "_FBLPromises",
       url: "https://github.com/prnd-ios/firebase-ios-sdk-xcframeworks/releases/download/12.8.0/_FBLPromises.xcframework.zip",
-      checksum: "3cb57a5ad9d97cd02cb13415c9dc4b89758cd4ccdd1765460bd678f87d0ae065"
+      checksum: "164a296b2dc67b053a337c430144ec70af4d71a3ad1ddbeefd6d5e754674ddd1"
     ),
     .binaryTarget(
       name: "_FirebaseABTesting",
       url: "https://github.com/prnd-ios/firebase-ios-sdk-xcframeworks/releases/download/12.8.0/_FirebaseABTesting.xcframework.zip",
-      checksum: "c5030c7579408605583597e7dfefb8acea60e524803878b713706cf932846b79"
+      checksum: "e0d66e73d3684e01ac0b8fbc7187f2edec6831a5ac49235aeec5124c34b6d5ac"
     ),
     .binaryTarget(
       name: "_FirebaseAnalytics",
       url: "https://github.com/prnd-ios/firebase-ios-sdk-xcframeworks/releases/download/12.8.0/_FirebaseAnalytics.xcframework.zip",
-      checksum: "6a334d2fb6cbe02446995b156223d064c1b25e4c979f7727866db43ac3ea38d0"
+      checksum: "702d56cc40b95c5ed65fa70d4df09a27eaa1ed3494e7395dba6d83a374e2fc38"
     ),
     .binaryTarget(
       name: "_FirebaseAppCheck",
       url: "https://github.com/prnd-ios/firebase-ios-sdk-xcframeworks/releases/download/12.8.0/_FirebaseAppCheck.xcframework.zip",
-      checksum: "277111409d2737074bd9598a8a360dc63c51e52fae3e89fdb252ac3d9da4c26f"
+      checksum: "34d0be03e5d60a64e1f65aceebaee8ebb755c78912c5cfd0d0a5030381f8fdd5"
     ),
     .binaryTarget(
       name: "_FirebaseAppCheckInterop",
       url: "https://github.com/prnd-ios/firebase-ios-sdk-xcframeworks/releases/download/12.8.0/_FirebaseAppCheckInterop.xcframework.zip",
-      checksum: "d8d5ba92b4eda81da802b14912d4d2fe0c2ff5f8e82f0e0ad8f15a6920cffee0"
+      checksum: "f550216435579694c889536754d999c2bf1a0176a118042a27f2e4a43e78c1d1"
     ),
     .binaryTarget(
       name: "_FirebaseCore",
       url: "https://github.com/prnd-ios/firebase-ios-sdk-xcframeworks/releases/download/12.8.0/_FirebaseCore.xcframework.zip",
-      checksum: "cb99ed646394e0bcd50b4df93e1794f8f363d446a19d2d9828c9a8cfea38eed1"
+      checksum: "b06b6d44eacd1bdd15221797fd2067625b53ca55a15f17e4f6eac9336ea8f4c2"
     ),
     .binaryTarget(
       name: "_FirebaseCoreExtension",
       url: "https://github.com/prnd-ios/firebase-ios-sdk-xcframeworks/releases/download/12.8.0/_FirebaseCoreExtension.xcframework.zip",
-      checksum: "28d943bf175a4d7fab5cdef04628fe23911c5f585236fd88fb697b7818623795"
+      checksum: "f8a2d49b5bb2f2e8e4aa2d9d27739d9d598a6b573cc64446202f0e0200c8411c"
     ),
     .binaryTarget(
       name: "_FirebaseCoreInternal",
       url: "https://github.com/prnd-ios/firebase-ios-sdk-xcframeworks/releases/download/12.8.0/_FirebaseCoreInternal.xcframework.zip",
-      checksum: "fa99b0c96ccafa6cfeb4d721102cfaf158291631ca5108e490e1c9acec8649a9"
+      checksum: "c661b3565a4a3fd0b7cea974ed1840e620e4e71494c5a290a485e7f7ada5be3c"
     ),
     .binaryTarget(
       name: "_FirebaseCrashlytics",
       url: "https://github.com/prnd-ios/firebase-ios-sdk-xcframeworks/releases/download/12.8.0/_FirebaseCrashlytics.xcframework.zip",
-      checksum: "85df5963ef55c7d9ab251478c1ecdf90d911f2ae03f5dd534cf1e2c00c7c72f7"
+      checksum: "5801178d0fd4cc0f8d3ad4d09c17f904389a1243ed218e7fff3dfc254849cc6a"
     ),
     .binaryTarget(
       name: "_FirebaseInstallations",
       url: "https://github.com/prnd-ios/firebase-ios-sdk-xcframeworks/releases/download/12.8.0/_FirebaseInstallations.xcframework.zip",
-      checksum: "15d1b22866422a8040b6ba8989362e9fe19b75bf7ab9be69fddf023c1537ff07"
+      checksum: "568df7e8417899ce8e1c9a818c693b7116257dd219d90f30ee098001193cf3d3"
     ),
     .binaryTarget(
       name: "_FirebaseMessaging",
       url: "https://github.com/prnd-ios/firebase-ios-sdk-xcframeworks/releases/download/12.8.0/_FirebaseMessaging.xcframework.zip",
-      checksum: "20b2826810c43da1e23d30934b938ccc16a59e759cbf9e505168a433b3bcca3c"
+      checksum: "87e54690491923b618622b53c7de02e7295223fe5cf7bbe9449662508fa26850"
     ),
     .binaryTarget(
       name: "_FirebasePerformance",
       url: "https://github.com/prnd-ios/firebase-ios-sdk-xcframeworks/releases/download/12.8.0/_FirebasePerformance.xcframework.zip",
-      checksum: "5bf45a693a29a04a3581a175270559f534cc98ddafd2786ffa2c6dccada8c428"
+      checksum: "94a1d6d9d0f9bb5b8d485ec892f48cb31734dffc0c41978b5b979ae56217ba69"
     ),
     .binaryTarget(
       name: "_FirebaseRemoteConfig",
       url: "https://github.com/prnd-ios/firebase-ios-sdk-xcframeworks/releases/download/12.8.0/_FirebaseRemoteConfig.xcframework.zip",
-      checksum: "99e1d510950348c745c08d8477e2f085eebf705b914de027e5d1b0b873c85b02"
+      checksum: "8f89dedbacb6fe9f444b23e6a1f2e306ac5deaf4410859b64879997df76d0b06"
     ),
     .binaryTarget(
       name: "_FirebaseRemoteConfigInterop",
       url: "https://github.com/prnd-ios/firebase-ios-sdk-xcframeworks/releases/download/12.8.0/_FirebaseRemoteConfigInterop.xcframework.zip",
-      checksum: "3445d647e82ecba76ee84a63190631a86cfbfb8a2ad2d478794afbb8d0abb095"
+      checksum: "b5e13cb7c33ada189781ae25a1f70d1f772190356a7dab0b6820f1127d7937d3"
     ),
     .binaryTarget(
       name: "_FirebaseSessions",
       url: "https://github.com/prnd-ios/firebase-ios-sdk-xcframeworks/releases/download/12.8.0/_FirebaseSessions.xcframework.zip",
-      checksum: "8f9fc2b0ca240ebb6694dd9eacb0f9d105fb51aa2403466dd3d23cb9574823af"
+      checksum: "7c8b5a14cec1bbb38acbd03bf3b028f74e9a47cefa339e683748ba10c5cad5c1"
     ),
     .binaryTarget(
       name: "_FirebaseSharedSwift",
       url: "https://github.com/prnd-ios/firebase-ios-sdk-xcframeworks/releases/download/12.8.0/_FirebaseSharedSwift.xcframework.zip",
-      checksum: "99917e53bdb964aab380aa2723b3d2741d49411342219e8d72eaf8147869f9e1"
+      checksum: "cd74231718888758d724a67dad8b7c81f1f3065e94f0bb5b9f046a16f68076d9"
     ),
     .binaryTarget(
       name: "_GoogleAdsOnDeviceConversion",
       url: "https://github.com/prnd-ios/firebase-ios-sdk-xcframeworks/releases/download/12.8.0/_GoogleAdsOnDeviceConversion.xcframework.zip",
-      checksum: "3214575709d78ce981aa3e1ebb32d5aad57796b67b588c52d145c6fbe0974749"
+      checksum: "dbaa35e2e52aeafc7613e1dae93ad6f6e7e50c40031fd4ee5ee8eb38a5729704"
     ),
     .binaryTarget(
       name: "_GoogleAppMeasurement",
       url: "https://github.com/prnd-ios/firebase-ios-sdk-xcframeworks/releases/download/12.8.0/_GoogleAppMeasurement.xcframework.zip",
-      checksum: "56c87fca247e1de2d28852b669812591462e3430c924b5f0ebee2c9e2ad7c1c3"
+      checksum: "51e0e7a550a3f806a75935b4c81e1d879f6b99d07d60acf9868f7c353a9245a1"
     ),
     .binaryTarget(
       name: "_GoogleAppMeasurementIdentitySupport",
       url: "https://github.com/prnd-ios/firebase-ios-sdk-xcframeworks/releases/download/12.8.0/_GoogleAppMeasurementIdentitySupport.xcframework.zip",
-      checksum: "888ce6a92ddffbacb8ef212ca04e65b7abb3d5318b65214cc68c4525391a8fba"
+      checksum: "a7403c377da1f44a43b7b762d559808a90a636656aa03d038a5f9fc54b4d58ce"
     ),
     .binaryTarget(
       name: "_GoogleDataTransport",
       url: "https://github.com/prnd-ios/firebase-ios-sdk-xcframeworks/releases/download/12.8.0/_GoogleDataTransport.xcframework.zip",
-      checksum: "f4bfc1cd8166832be9b45fddcc4b8f4ef0e041389830e89e356e3dbed31eff25"
+      checksum: "02e2a02e3e8ea0a6e550d877a4c7f956f26da0129ea0fed85aa586da33ad0300"
     ),
     .binaryTarget(
       name: "_GoogleUtilities",
       url: "https://github.com/prnd-ios/firebase-ios-sdk-xcframeworks/releases/download/12.8.0/_GoogleUtilities.xcframework.zip",
-      checksum: "7d1caf0bbc2d7762d3216bce908a9abf13a7a627b1cd500647845feae9d5420e"
+      checksum: "ce957bb8e8aa5fa5de77636b3e51ae77a3fb9224e0216c41b151a06adfe428db"
     ),
     .binaryTarget(
       name: "_Promises",
       url: "https://github.com/prnd-ios/firebase-ios-sdk-xcframeworks/releases/download/12.8.0/_Promises.xcframework.zip",
-      checksum: "884627a581a5461c9ee42d58f906e9915efd2f0ae75a2f136488be6552c843c2"
+      checksum: "8f16d15146429ab1139ba122d49a3c934c8b597f292f913e8045c8fbfe39ffa2"
     ),
     .binaryTarget(
       name: "_nanopb",
       url: "https://github.com/prnd-ios/firebase-ios-sdk-xcframeworks/releases/download/12.8.0/_nanopb.xcframework.zip",
-      checksum: "2271f9ff379edec56c3fbd3b10627cc4d4c2bb4b130f771a28dfd8b975dcb817"
+      checksum: "043f88fb5696aa4f35d4bd9a3c590d8f53568225e56a8fff871fc8b54de41639"
     )
   ]
 )
